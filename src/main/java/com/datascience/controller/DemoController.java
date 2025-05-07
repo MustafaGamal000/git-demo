1440f7282c9ce8441d9217db597c67dcd71228e6
MustafaGamal000 <mustafa.gamal000@gmail.com>
2025-05-07 14:27:24 +0300

    add Controller Class

---
+++ src/main/java/com/datascience/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.datascience.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/hi")
+    public String sayHi(){
+        return "Hi..!";
+    }
+}
