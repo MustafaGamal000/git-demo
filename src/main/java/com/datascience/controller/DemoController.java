1782884df27caa522e49ff62738172cd94825044
MustafaGamal000 <mustafa.gamal000@gmail.com>
2025-05-07 14:37:40 +0300

    integrate service class with controller class

--- src/main/java/com/datascience/controller/DemoController.java
+++ src/main/java/com/datascience/controller/DemoController.java
@@ -1,12 +1,16 @@
 package com.datascience.controller;
 
+import com.datascience.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+    @Autowired
+    DemoService demoService;
     @GetMapping("/hi")
     public String sayHi(){
-        return "Hi..!";
+        return demoService.sayHi();
     }
 }
